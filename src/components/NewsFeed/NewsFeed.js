02268788cd119e0da792fe26a4ee538a29d4a736
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-31 22:17:18 +0600

    change in newsfeed

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -9,7 +9,6 @@ const NewsFeed = () => {
                 <img src={img} className='container-fluid' alt="" />
                 <p className='text-start text-white p-2'>Since the establishment, Port City International University has been focusing on assisting the students in facing the challenges of the ever advancing world. PCIU is a platform where students can rise to the highest level of their capability. It provides an outstanding and supportive environment for both undergraduate and postgraduate students. A talented and dedicated group of academics provide guidance and tutelage the students need to pursue their research and academic goals. The dynamic teaching and learning environment of PCIU brims with talent, creativity and international connections. PCIU has been doing excellent community services by engaging the students</p>
             </div>
-
             <div className='card bg-dark border w-25  mx-auto pt-4'>
                 <form action="">
                     <h6 className='text-white fw-bold text-end me-5'>Name</h6>
