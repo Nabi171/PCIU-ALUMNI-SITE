5d1354737833528c2c1914a6eadc1a7d307d2cf9
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-01 18:28:12 +0600

    changes in post section

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,47 +1,18 @@
 import React from 'react';
 import img from '../images/imagePciu.jpg';
-import { useForm } from 'react-hook-form';
+import NewsForm from './NewsForm';
 const NewsFeed = () => {
-    const { register, handleSubmit } = useForm();
-    const onsubmit = data => {
-        const url = `http://localhost:4000/newmembers`;
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => res.json())
-            .then(result => {
-                console.log(result);
-            })
-        alert('your id is succesfully added');
-        window.location.reload(false);
-    }
+
     return (
-        <div className='container bg-black rounded'>
+        <div className='container bg-black rounded mb-4 p-4'>
             <div>
                 <h5 className='text-white fw-bold pt-2'>Wellcome to PCIU News Feed</h5>
                 <hr className='text-white w-50 mx-auto' />
-                <img src={img} className='container-fluid' alt="" />
+                <img src={img} className='container-fluid rounded' alt="" />
                 <p className='text-start text-white p-2'>Since the establishment, Port City International University has been focusing on assisting the students in facing the challenges of the ever advancing world. PCIU is a platform where students can rise to the highest level of their capability. It provides an outstanding and supportive environment for both undergraduate and postgraduate students. A talented and dedicated group of academics provide guidance and tutelage the students need to pursue their research and academic goals. The dynamic teaching and learning environment of PCIU brims with talent, creativity and international connections. PCIU has been doing excellent community services by engaging the students</p>
             </div>
-            <div className='card bg-dark border w-25  mx-auto pt-4'>
-                <form onsubmit={handleSubmit(onsubmit)} action="">
-                    <h6 className='text-white fw-bold text-end me-5'>Name</h6>
-                    <input {...register("name", { required: true, maxLength: 20 })} type="text" />
-                    <br /><br />
-                    <h6 className='text-white fw-bold text-end me-5'>Name</h6>
-                    <input {...register("name", { required: true, maxLength: 20 })} type="text" />
-                    <br /><br />
-                    <h6 className='text-white fw-bold text-end me-5'>Write Your Post</h6>
-                    <textarea {...register("name", { required: true, maxLength: 20 })} required name="message" className='p-3' />
-                    <br />
-                    <input className='btn btn-outline-info mt-3 mb-3' type="submit" value="Add Post" />
+            <NewsForm></NewsForm>
 
-                </form>
-            </div>
         </div>
     );
 };
---
+++ src/components/NewsFeed/NewsForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+const NewsForm = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:4000/posts`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+        alert('your id is succesfully added');
+        window.location.reload(false);
+
+    };
+    return (
+        <div>
+            <div className='card bg-dark border w-75  mx-auto p-5 '>
+                <form onSubmit={handleSubmit(onSubmit)} className=" text-center  ">
+
+                    <div className="inputs-form">
+                        <h6 className='text-white fw-bold text-start '>Name</h6>
+                        <input className="form-input" required type="text" placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+
+                        <h6 className='text-white fw-bold text-start mt-4'>Department Name</h6>
+                        <input placeholder='department name' required {...register("department")} className="form-input" type="text" />
+
+                        <h6 className='text-white fw-bold text-start mt-4'>Batch Name</h6>
+                        <input placeholder='batch name' required {...register("Supply")} className="form-input" type="text" />
+
+
+
+
+                        <h6 className='text-white fw-bold text-start mt-4'>Message</h6>
+                        <textarea {...register("message", { required: true, maxLength: 20 })} required name="message" className='p-3 w-100' />
+
+
+
+
+
+                        <button
+                            className='btn btn-outline-light mt-5' type="submit">Add Post
+                         {/* <ToastContainer /> */}
+
+
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default NewsForm;
