d3621b8053b46bdc48f93f313de86eee6431ecad
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-09 14:35:19 +0600

    changes in proksdl

--- src/components/NewsFeed/NewsHeadPanel.js
+++ src/components/NewsFeed/NewsHeadPanel.js
@@ -5,35 +5,29 @@ const NewsHeadPanel = () => {
         <div>
             <h3 className='text-white mx-auto my-2'>Our Estimed Faculty Members</h3>
             <div className='row'>
-                <div className='col-sm-3 text-white'>
-                    <img className='w-50 imgNewsHead' src="https://scontent.fcgp2-1.fna.fbcdn.net/v/t39.30808-6/313223176_665749175230992_1231809695670536887_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHWUfJJ2FI0CgZk_GCRzqrEc0JGaKheEuNzQkZoqF4S425tPd1k6BVeSXBbrv9J4H8-TSAwhXHZ3wUasAdPGGzr&_nc_ohc=dYXZPQ8OLnMAX9A-drS&_nc_ht=scontent.fcgp2-1.fna&oh=00_AfA5Dl6TZc4-ia5bieVBPsyfDj16fEB7pIHivsIrLoIxpg&oe=63707B4B" alt="" />
+
+                <div className='col-sm-4 text-white'>
+                    <img className='w-50 imgNewsHead' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR30ejsVhxfVnuWp1OQkiogpMYmXWDPir_JloqYP3atc722gRCMFbXSOYzL7yC0ncA4G2Y&usqp=CAU" alt="" />
                     <h6>Dr. Muhammed Monjurl Islalm</h6>
-                    <p><i>Phd in Machine Learning</i></p>
+                    <p><i>Phd in Data Structure & Machine Learning</i></p>
+                    <p><i>Harvard</i></p>
+
+                </div>
+                <div className='col-sm-4 text-white'>
+                    <img className='w-50 imgNewsHead' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc4P1OxYpn3r7Rj1lXBHsbx6ZnppaXOO_cEh2zhappwuGUbUSqC5b4-I7IhUbKQrPvKlQ&usqp=CAU" alt="" />
+                    <h6>Dr. Muhammed Monjurl Islalm</h6>
+                    <p><i>Phd in Data Science</i></p>
+                    <p><i>Oxford</i></p>
+
+                </div>
+                <div className='col-sm-4 text-white'>
+                    <img className='w-50 imgNewsHead' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoGfRA7BOHEIFxtFzTNyqfWYc-zYSPaTwuHRW072bLhLoeO6c0FI2KuZAULPCUQqi8B8U&usqp=CAU" alt="" />
+                    <h6>Dr. Muhammed Monjurl Islalm</h6>
+                    <p><i>Phd in Web.30 & BlockChain-Application</i></p>
                     <p><i>MIT</i></p>
 
                 </div>
-                <div className='col-sm-3 text-white'>
-                    <img className='w-50 imgNewsHead' src="https://scontent.fcgp2-1.fna.fbcdn.net/v/t39.30808-6/313223176_665749175230992_1231809695670536887_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHWUfJJ2FI0CgZk_GCRzqrEc0JGaKheEuNzQkZoqF4S425tPd1k6BVeSXBbrv9J4H8-TSAwhXHZ3wUasAdPGGzr&_nc_ohc=dYXZPQ8OLnMAX9A-drS&_nc_ht=scontent.fcgp2-1.fna&oh=00_AfA5Dl6TZc4-ia5bieVBPsyfDj16fEB7pIHivsIrLoIxpg&oe=63707B4B" alt="" />
-                    <h6>Dr. Muhammed Monjurl Islalm</h6>
-                    <p><i>Phd in Machine Learning</i></p>
-                    <p><i>MIT</i></p>
-
-                </div>
-                <div className='col-sm-3 text-white'>
-                    <img className='w-50 imgNewsHead' src="https://scontent.fcgp2-1.fna.fbcdn.net/v/t39.30808-6/313223176_665749175230992_1231809695670536887_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHWUfJJ2FI0CgZk_GCRzqrEc0JGaKheEuNzQkZoqF4S425tPd1k6BVeSXBbrv9J4H8-TSAwhXHZ3wUasAdPGGzr&_nc_ohc=dYXZPQ8OLnMAX9A-drS&_nc_ht=scontent.fcgp2-1.fna&oh=00_AfA5Dl6TZc4-ia5bieVBPsyfDj16fEB7pIHivsIrLoIxpg&oe=63707B4B" alt="" />
-                    <h6>Dr. Muhammed Monjurl Islalm</h6>
-                    <p><i>Phd in Machine Learning</i></p>
-                    <p><i>MIT</i></p>
-
-                </div>
-                <div className='col-sm-3 text-white'>
-                    <img className='w-50 imgNewsHead' src="https://scontent.fcgp2-1.fna.fbcdn.net/v/t39.30808-6/313223176_665749175230992_1231809695670536887_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHWUfJJ2FI0CgZk_GCRzqrEc0JGaKheEuNzQkZoqF4S425tPd1k6BVeSXBbrv9J4H8-TSAwhXHZ3wUasAdPGGzr&_nc_ohc=dYXZPQ8OLnMAX9A-drS&_nc_ht=scontent.fcgp2-1.fna&oh=00_AfA5Dl6TZc4-ia5bieVBPsyfDj16fEB7pIHivsIrLoIxpg&oe=63707B4B" alt="" />
-                    <h6>Dr. Muhammed Monjurl Islalm</h6>
-                    <p><i>Phd in Machine Learning</i></p>
-                    <p><i>MIT</i></p>
-
-                </div>
-
+             
             </div>
             <hr className='text-white' />
         </div>
