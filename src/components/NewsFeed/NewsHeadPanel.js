f19323c3fe8eff32c9638fbd8983d442562c4adc
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-09 13:43:41 +0600

    changes in pid

---
+++ src/components/NewsFeed/NewsHeadPanel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NewsHeadPanel = () => {
+    return (
+        <div>
+            <h3 className='text-white mx-auto my-2'>Our Estimed Faculty Members</h3>
+            <div className='row'>
+                <div className='col-sm-3'>
+                    <img className='w-25' src="https://scontent.fcgp2-1.fna.fbcdn.net/v/t39.30808-6/313223176_665749175230992_1231809695670536887_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHWUfJJ2FI0CgZk_GCRzqrEc0JGaKheEuNzQkZoqF4S425tPd1k6BVeSXBbrv9J4H8-TSAwhXHZ3wUasAdPGGzr&_nc_ohc=dYXZPQ8OLnMAX9A-drS&_nc_ht=scontent.fcgp2-1.fna&oh=00_AfA5Dl6TZc4-ia5bieVBPsyfDj16fEB7pIHivsIrLoIxpg&oe=63707B4B" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewsHeadPanel;
