8324af61c2a82f49a834bdfb5e412c0652ddf7a1
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-09 13:57:45 +0600

    changes in some potr

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import img from '../images/imagePciu.jpg';
 import StuNews from './StuNews';
+import NewsHeadPanel from './NewsHeadPanel';
 const NewsFeed = () => {
 
     return (
@@ -12,6 +13,7 @@ const NewsFeed = () => {
                 <p className='text-start text-white p-2'>Since the establishment, Port City International University has been focusing on assisting the students in facing the challenges of the ever advancing world. PCIU is a platform where students can rise to the highest level of their capability. It provides an outstanding and supportive environment for both undergraduate and postgraduate students. A talented and dedicated group of academics provide guidance and tutelage the students need to pursue their research and academic goals. The dynamic teaching and learning environment of PCIU brims with talent, creativity and international connections. PCIU has been doing excellent community services by engaging the students</p>
             </div>
             <hr className='text-white' />
+            <NewsHeadPanel></NewsHeadPanel>
             <marquee behavior="" direction="" className='text-white'>
                 Wellcome to Port City International University students News Section !!!  Wellcome to Port City International University students News Section !!!
                 Wellcome to Port City International University students News Section !!!  Wellcome to Port City International University students News Section !!!
@@ -29,6 +31,7 @@ const NewsFeed = () => {
                 <h6 className='text-white text-end'>Id No:1234</h6>
             </div>
             {/* <NewsForm></NewsForm> */}
+           
             <StuNews></StuNews>
         </div>
     );
